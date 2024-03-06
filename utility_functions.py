39fe1c22bb7dc7f9199f92270dd8abd07d9f93e7
Otto Chamo <cheleyotto98@gmail.com>
2024-03-06 00:32:08 -0600

    feat: create utility_functions.py and correlation_plot_args.py

---
+++ utility_functions.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from correlation_plot_args import CorrelationPlotArgs
+
+def calculateRangeForColumn(ds: pd.DataFrame, columnName: str):
+    max = ds[columnName]['max']
+    min = ds[columnName]['min']
+    return max - min
+
+
+def plot_columns(ds: pd.DataFrame):
+    columns = ds.columns
+    for i, col in enumerate(columns):
+        plt.figure(i)
+        sns.histplot(ds[col], kde=True)
+
+def calculateCorrelation(x: pd.Series, y: pd.Series):
+    return np.corrcoef(x, y)
+
+def plotScatterPlot(args: CorrelationPlotArgs):
+    plt.scatter(args.x, args.y, alpha=0.5)
+    plt.title(f"{args.title}. Correlation: {args.correlation:.4f}")
+    plt.show()
+
+def printCorrelation(colName: str, correlation: float):
+    print(f"Correlation for {colName} = {correlation:.4f}")
